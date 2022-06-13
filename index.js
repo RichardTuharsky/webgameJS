9e21ba2398a0f34562b56dd8f6b5a6a5eb5f42d7
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-13 19:52:58 +0200

    projectile

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Player {
         this.color = color 
     }
     draw() {
-        c.beginPath()
+        c.beginPath() 
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
@@ -36,7 +36,11 @@ class Projectile {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
-
+    }
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
     }
 }
 
@@ -48,10 +52,24 @@ const player = new Player(x, y, 30, 'blue')
 
 player.draw()
 
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+}
+
 //after clicking do action, this event knows exactly when my mouse was 
 addEventListener('click', (event) =>
      {
-    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
+        x: 1, y: 1
+         })
     )
-    projectile.draw()
 })
+
+
+animate()
